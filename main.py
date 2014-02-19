37c8b99b34c942d373ee4ec13fce22cc7f7e3112
Sandy Lam <sandylamwork@gmail.com>
2014-02-19 14:00:53 +0800

    Finished Demo

--- main.py
+++ main.py
@@ -1,25 +1,38 @@
-#!/usr/bin/env python
-#
-# Copyright 2007 Google Inc.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
-import webapp2
 
+import webapp2
+from google.appengine.ext import ndb    
+
+class HandleForm(webapp2.RequestHandler):
+    def post(self):
+        name = self.request.get('name')
+        self.response.write('Hello %s!'%name)
+        saveForm(self)
+
+class Form(ndb.Model):
+    name = ndb.StringProperty()
+    email = ndb.StringProperty()
+    age = ndb.IntegerProperty()
+    date = ndb.DateTimeProperty(auto_now_add=True)
+
+def saveForm(self):    
+    age = int(self.request.get('age'))
+    name = self.request.get('name')
+    email = self.request.get('email')        
+    if email:
+        # use email as key name
+        form = Form(key=ndb.Key('Form', email), name=name,age=age)
+    else:
+        form = Form(name=name,age=age)        
+    form.put()    
+    
+    self.response.write('<br>Your form is saved')
+    
 class MainHandler(webapp2.RequestHandler):
     def get(self):
-        self.response.write('Hello world!')
+        self.response.write('Hello!<br>')
+        self.response.write('Start your <a href="/start">form</a>')
 
 app = webapp2.WSGIApplication([
-    ('/', MainHandler)
+    ('/', MainHandler),
+    ('/submit', HandleForm)
 ], debug=True)
